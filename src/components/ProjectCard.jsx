1911be448c6158dcba913554a6db637969ee5079
Biplab Khanal <khanalbiplab@gmail.com>
2024-08-22 16:05:38 +0545

    UI modified

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,31 @@
 import { Col } from "react-bootstrap";
-const ProjectCard = ({ title, description, imgUrl }) => {
+const ProjectCard = ({ title, description, imgUrl, githubLink }) => {
+  const handleRedirect = () => {
+    window.open(githubLink, "_blank");
+  };
+
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+    <>
+      <Col size={12} sm={6} md={6} className="d-md-block d-none">
+        <div className="proj-imgbx " onClick={handleRedirect}>
+          <img style={{ height: "350px" }} src={imgUrl} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
-      </div>
-    </Col>
+      </Col>
+
+      <Col size={12} sm={6} md={6} className=" d-lg-none d-md-none d-block">
+        <div className="proj-imgbx" onClick={handleRedirect}>
+          <img style={{ height: "260px" }} src={imgUrl} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
+        </div>
+      </Col>
+    </>
   );
 };
 export default ProjectCard;
